0bd541453990cf8b86a18d4ff0fa44e434b34ade
Chris Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2025-01-31 00:50:45 +0100

    Finalisation de la mise en place du front de la partie authentification

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/intervenants");
+    }
+  }, [session, router]);
+  return (
+    <>
+      <div>
+        <div className="mt-7 mx-96 bg-white border border-gray-200 rounded-xl shadow-sm">
+          <div className="p-4 sm:p-7">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800">
+                Mot de passe oublié?
+              </h1>
+              <p className="mt-2 text-sm text-gray-600">
+                Vous vous souvenez de votre mot de passe ?{" "}
+                <a
+                  className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium"
+                  href="/login"
+                >
+                  Connectez-vous ici
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-5">
+              {/* <!-- Form --> */}
+              <form>
+                <div className="grid gap-y-4">
+                  {/* <!-- Form Group --> */}
+                  <div>
+                    <label htmlFor="email" className="block text-sm mb-2">
+                      Adresse email
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                        required
+                        aria-describedby="email-error"
+                      />
+                      <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                        <svg
+                          className="size-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p
+                      className="hidden text-xs text-red-600 mt-2"
+                      id="email-error"
+                    >
+                      Please include a valid email address so we can get back to
+                      you
+                    </p>
+                  </div>
+                  {/* <!-- End Form Group --> */}
+
+                  <button
+                    type="submit"
+                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  >
+                    Réinitialiser le mot de passe
+                  </button>
+                </div>
+              </form>
+              {/* <!-- End Form --> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
