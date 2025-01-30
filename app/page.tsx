ad9a13439636a4ebd79f52a422b81e378f114147
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2025-01-30 11:38:21 +0100

    Ajout de la prise en charge de l'authentification avec Google et refactorisation des composants de connexion

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/lib/auth";
-import { LoginButton, LogoutButton } from "./AuthButtons";
+import { LoginButton, LogoutButton } from "./components/sign-in";
+import SignIn from "./components/SignIn";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await auth();
@@ -7,12 +9,20 @@ export default async function Home() {
     <>
       <main>
         <h1>Hello, world!</h1>
+        {session?.user ? (
+          "Authenticated " + session?.user.email + " " + session?.user.name
+        ) : (
+          <SignIn />
+        )}
+
+        {/*
         <i>
           {session?.user
             ? "Authenticated " + session?.user.email
             : "Unauthenticated"}
         </i>
         <div>{session?.user ? <LogoutButton /> : <LoginButton />}</div>
+        */}
       </main>
     </>
   );
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./prisma";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GitHubProvider({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
   ],
 });
