31982eb21fbd478c6a628cecb83a50d151930f38
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2025-01-30 11:45:17 +0100

    Ajout de la gestion des rôles d'utilisateur dans la session et mise à jour des migrations de base de données

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,4 +11,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    },
+  },
 });
